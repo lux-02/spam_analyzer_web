174bf21ef1dc8d5311788c73ed07860deb7f11e3
lux00 <oys128950@gmail.com>
2025-05-20 17:55:58 +0900

    update main bg color

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
 import Link from "next/link";
-import Footer from "../components/ui/Footer";
+import { useRouter } from "next/router";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
@@ -9,35 +8,43 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>DarkWinterLab</title>
         <meta name="description" content="DarkWinterLab" />
         <link rel="icon" href="/favicon.ico" />
+        <style jsx global>{`
+          body {
+            background-color: #121212;
+          }
+        `}</style>
       </Head>
 
-      {/* 메인 콘텐츠 */}
-      <main className={styles.main}>
-        {/* 메인 이미지 */}
-        <div className={styles.imageContainer}>
-          <Image
-            src="/main.svg"
-            alt="DarkWinterLab logo"
-            width={324}
-            height={411}
-            priority
-            className="w-full h-auto"
-          />
-        </div>
+      <div className={styles.container}>
+        {/* 메인 콘텐츠 */}
+        <main className={styles.main}>
+          {/* 메인 이미지 */}
+          <div className={styles.imageContainer}>
+            <Image
+              src="/main.svg"
+              alt="DarkWinterLab logo"
+              width={324}
+              height={411}
+              priority
+              className="w-full h-auto"
+            />
+          </div>
 
-        {/* 네이버 서비스 카드 링크 */}
-        <Link href="/naver" className={styles.card}>
-          <h2>네이버 메일 분석기 &rarr;</h2>
-          <p>
-            네이버 메일의 원문 데이터를 붙여넣고 스팸/피싱 여부를 분석해보세요.
-          </p>
-        </Link>
-      </main>
-    </div>
+          {/* 네이버 서비스 카드 링크 */}
+          <Link href="/naver" className={styles.card}>
+            <h2>네이버 메일 분석기 &rarr;</h2>
+            <p>
+              네이버 메일의 원문 데이터를 붙여넣고 스팸/피싱 여부를
+              분석해보세요.
+            </p>
+          </Link>
+        </main>
+      </div>
+    </>
   );
 }
